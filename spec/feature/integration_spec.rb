1a4f32480fa3ab88c2f2c4135063092f49d4008b
TylerNichols280 <tnichols280@gmail.com>
2022-09-15 16:39:18 -0500

    U/I test almost complete

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe 'Creating a book', type: :feature do
 
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J.K. Rowling'
-    fill_in 'Price', with: '4.19'
-    fill_in 'Published Date', with: '1997-01-07'
+    fill_in 'Price', with: 4.19
+    fill_in 'Published date', with: '1997-01-07'
   
     click_on 'Create Book'
     visit books_path
@@ -16,7 +16,6 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('J.K. Rowling')
     expect(page).to have_content(4.19)
     expect(page).to have_content('1997-01-07')
-
   end
   
   scenario 'invalid title, valid otherwise' do
@@ -24,12 +23,11 @@ RSpec.describe 'Creating a book', type: :feature do
 
     fill_in 'Title', with: nil
     fill_in 'Author', with: 'J.K. Rowling'
-    fill_in 'Price', with: '4.19'
-    fill_in 'Published Date', with: '1997-01-07'
+    fill_in 'Price', with: 4.19
+    fill_in 'Published date', with: '1997-01-07'
 
     click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('prohibited this book from being saved')
   end
 
   scenario 'invalid author, valid otherwise' do
@@ -37,12 +35,11 @@ RSpec.describe 'Creating a book', type: :feature do
     
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: nil
-    fill_in 'Price', with: '4.19'
-    fill_in 'Published Date', with: '1997-01-07'
+    fill_in 'Price', with: 4.19
+    fill_in 'Published date', with: '1997-01-07'
 
     click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('prohibited this book from being saved')
   end
 
   scenario 'invalid price, valid otherwise' do
@@ -51,11 +48,10 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J.K. Rowling'
     fill_in 'Price', with: nil
-    fill_in 'Published Date', with: Date.new(1997,1,7)
+    fill_in 'Published date', with: Date.new(1997,1,7)
 
     click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('prohibited this book from being saved')
   end
 
   scenario 'invalid published date, valid otherwise' do
@@ -63,11 +59,10 @@ RSpec.describe 'Creating a book', type: :feature do
     
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'J.K. Rowling'
-    fill_in 'Price', with: '4.19'
-    fill_in 'Published Date', with: nil
+    fill_in 'Price', with: 4.19
+    fill_in 'Published date', with: nil
 
     click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('prohibited this book from being saved')
   end
 end
